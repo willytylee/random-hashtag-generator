db4b230634ef02e3c626e026e5b055e893878089
Willy Lee <willytylee@gmail.com>
2022-08-21 04:01:58 +0800

    fix eslint

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button, Slider, Grid } from "@mui/material";
 import "./App.scss";
 import { CategoryForm } from "./components/CategoryForm";
 import { HashtagTextarea } from "./components/HashtagTextarea";
-import { data } from "./Data.js";
+import { data } from "./Data";
 
 const App = () => {
   const [inputFields, setInputFields] = useState(data);
 
   const handleFormChange = (i, e) => {
-    const data = inputFields;
-    data["hashtags"][i][e.target.name] = e.target.value;
-    setInputFieldsFn(data);
+    inputFields["hashtags"][i][e.target.name] = e.target.value;
+    setInputFieldsFn(inputFields);
     submit();
   };
 
   const handleCheckboxChange = (i, e) => {
-    const data = inputFields;
-    data["hashtags"][i][e.target.name] = e.target.checked;
-    setInputFieldsFn(data);
+    inputFields["hashtags"][i][e.target.name] = e.target.checked;
+    setInputFieldsFn(inputFields);
     submit();
   };
 
   const handleSliderChange = (e) => {
-    const data = inputFields;
-    data["maxLimit"] = e.target.value;
-    setInputFieldsFn(data);
+    inputFields["maxLimit"] = e.target.value;
+    setInputFieldsFn(inputFields);
     submit();
   };
 
   const handlefinalHashtagFieldChange = (e) => {
-    const data = inputFields;
-    data["finalHashtags"] = e.target.value;
-    setInputFieldsFn(data);
+    inputFields["finalHashtags"] = e.target.value;
+    setInputFieldsFn(inputFields);
   };
 
   const addCategory = () => {
-    const data = inputFields;
     const newfield = {
       selected: false,
       category: "",
@@ -44,16 +39,15 @@ const App = () => {
       compulsory: "",
       count: 0,
     };
-    data["hashtags"] = [...inputFields["hashtags"], newfield];
-    setInputFieldsFn(data);
+    inputFields["hashtags"] = [...inputFields["hashtags"], newfield];
+    setInputFieldsFn(inputFields);
   };
 
   const remove = (i, e) => {
     e.preventDefault();
 
-    const data = inputFields;
-    data.hashtags.splice(i, 1);
-    setInputFieldsFn(data);
+    inputFields.hashtags.splice(i, 1);
+    setInputFieldsFn(inputFields);
     submit();
   };
 
@@ -63,7 +57,6 @@ const App = () => {
     let standardFullArr = [];
     let standardReducedArr = [];
     let finalHashtags = [];
-    let totalCount = 0;
 
     inputFields.hashtags.forEach((value) => {
       // Group the compulsory hashtags
@@ -73,7 +66,7 @@ const App = () => {
         compulsoryItems &&
           (compulsoryArr = [...new Set(compulsoryArr.concat(compulsoryItems))]);
 
-        //--------------------------------------
+        // --------------------------------------
 
         // Group the standard hashtags
         const standardItems = standard.match(/#([^ ]+)/g);
@@ -108,15 +101,14 @@ const App = () => {
 
     finalHashtags = finalHashtags.sort(() => 0.5 - Math.random()).join(" ");
 
-    const data = inputFields;
-    data["finalHashtags"] = finalHashtags;
-    setInputFieldsFn(data);
+    inputFields["finalHashtags"] = finalHashtags;
+    setInputFieldsFn(inputFields);
   };
 
-  const setInputFieldsFn = (data) => {
+  const setInputFieldsFn = (_data) => {
     setInputFields((prevState) => ({
       ...prevState,
-      ...data,
+      ..._data,
     }));
   };
 
@@ -134,18 +126,16 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {inputFields?.hashtags?.map((input, i) => {
-            return (
-              <CategoryForm
-                input={input}
-                key={i}
-                i={i}
-                remove={remove}
-                handleFormChange={handleFormChange}
-                handleCheckboxChange={handleCheckboxChange}
-              />
-            );
-          })}
+          {inputFields?.hashtags?.map((input, i) => (
+            <CategoryForm
+              input={input}
+              key={i}
+              i={i}
+              remove={remove}
+              handleFormChange={handleFormChange}
+              handleCheckboxChange={handleCheckboxChange}
+            />
+          ))}
         </tbody>
       </table>
       <Button onClick={addCategory}>Create Category</Button>
@@ -156,7 +146,7 @@ const App = () => {
             rows={4}
             value={inputFields.finalHashtags}
             onChange={(e) => handlefinalHashtagFieldChange(e)}
-          ></HashtagTextarea>
+          />
         </Grid>
         <Grid item xs={2}>
           <Button
